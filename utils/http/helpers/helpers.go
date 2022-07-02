dde3ce42e054a3d0f21e67f330f554ea0f8062cf
Volodymyr Tkach <vladimirok5959@gmail.com>
2022-07-02 03:41:03 +0300

    RespondAsBadRequest, RespondAsMethodNotAllowed

--- utils/http/helpers/helpers.go
+++ utils/http/helpers/helpers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 
 	"github.com/vladimirok5959/golang-server-sessions/session"
 )
@@ -21,10 +22,33 @@ func MinifyHtmlCode(str string) string {
 	return str
 }
 
+// Example:
+//
+// sess := helpers.SessionStart(w, r)
+//
+// defer sess.Close()
 func SessionStart(w http.ResponseWriter, r *http.Request) (*session.Session, error) {
 	sess, err := session.New(w, r, "/tmp")
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		log.Printf("%s\n", err.Error())
 	}
 	return sess, err
+}
+
+func RespondAsBadRequest(w http.ResponseWriter, r *http.Request, err error) {
+	if err != nil {
+		log.Printf("%s\n", err.Error())
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		if _, e := w.Write([]byte(`{"error":"` + strconv.Quote(err.Error()) + `"}`)); e != nil {
+			log.Printf("%s\n", e.Error())
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func RespondAsMethodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
 }
